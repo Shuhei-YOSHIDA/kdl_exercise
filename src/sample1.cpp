bea64cf0dab4faaad2d34a418f65bc14b5645f99
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2019-12-20 22:08:38 +0900

    added files

---
+++ src/sample1.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file sample1.cpp
+ * @breaf Sample of KDL::Tree, KDL::Chain, FK
+ */
+
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "sample1");
+  return 0;
+}
